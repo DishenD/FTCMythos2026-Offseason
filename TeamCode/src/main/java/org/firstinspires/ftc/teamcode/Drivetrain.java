f0caf4dc77765fa9c3c9ccfc0b64340f05e9ec28
Dishen <ajsty81@gmail.com>
2026-02-14 12:35:02 -0500

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Drivetrain {
+
+    private DcMotorEx rightRear;
+
+    private Follower follower;
+}
